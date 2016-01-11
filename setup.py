ea555c2b46638456f151bf65d8bae620539c059f
Martin Kuehnhausen <martin.kuehnhausen@us.ibm.com>
2016-01-10 22:26:22 -0600

    Added pytest and coverage integration (closes #2)

--- setup.py
+++ setup.py
@@ -1,14 +1,21 @@
+import sys
+
 from setuptools import setup, find_packages
 
 import versioneer
 
+
+needs_pytest = {"pytest", "test", "ptr"}.intersection(sys.argv)
+pytest_runner = ["pytest-runner"] if needs_pytest else []
 
 setup(
     name = "c4-policyengine",
     version = versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
     packages = find_packages(),
-    install_requires = ["c4-system", "c4-utils"],
+    install_requires = ["c4-systemmanager", "c4-utils"],
+    setup_requires=[] + pytest_runner,
+    tests_require=["pytest", "pytest-cov"],
     author = "IBM",
     author_email = "",
     description = "This is a collection of policy engine modules for project C4",
