f74750613ec47576c10617d5422bd43c5fdf3f4c
Patrick J. Casteel <pjcastee@us.ibm.com>
2017-07-14 09:45:07 -0500

    Add instrumentation support to actions. (#16)

--- c4/policyengine/actions/devices.py
+++ c4/policyengine/actions/devices.py
@@ -17,7 +17,7 @@ class Start(Action):
 
     id = "devices.start"
 
-    def perform(self, node, fullDeviceName):
+    def perform(self, node, fullDeviceName, isRecovery=False):
         """
         Sends a start message to a device
 
@@ -25,12 +25,16 @@ class Start(Action):
         :type node: str
         :param fullDeviceName: fully qualified device manager name
         :type fullDeviceName: device
+        :prarm isRecovery: is this for HA recovery
+        :type isRecovery: bool
         """
         address = socket.gethostname().split(".")[0]
         client = RouterClient(address)
 
         self.log.debug("Sending start request to %s/%s", node, fullDeviceName)
-        client.forwardMessage(Operation("{0}/{1}".format(node, fullDeviceName), "start"))
+        client.forwardMessage(Operation("{0}/{1}".format(node, fullDeviceName),
+                                        "start",
+                                        isRecovery=isRecovery))
         return True
 
 @ClassLogger
@@ -41,7 +45,7 @@ class Restart(Action):
 
     id = "devices.restart"
 
-    def perform(self, node, fullDeviceName):
+    def perform(self, node, fullDeviceName, isRecovery=False):
         """
         Sends a restart message to a device
 
@@ -49,6 +53,8 @@ class Restart(Action):
         :type node: str
         :param fullDeviceName: fully qualified device manager name
         :type fullDeviceName: device
+        :prarm isRecovery: is this for HA recovery
+        :type isRecovery: bool
         """
         address = socket.gethostname().split(".")[0]
         client = RouterClient(address)
@@ -62,5 +68,25 @@ class Restart(Action):
             return False
 
         self.log.debug("Sending restart request to %s/%s", resolvedNode, fullDeviceName)
-        client.forwardMessage(Operation("{0}/{1}".format(resolvedNode, fullDeviceName), "restart"))
+        client.forwardMessage(Operation("{0}/{1}".format(resolvedNode, fullDeviceName),
+                                        "restart",
+                                        isRecovery=isRecovery))
         return True
+
+class RecoveryRestart(Restart):
+    """
+    Sends a restart message to a device with the intention of HA recovery.
+    """
+
+    id = "devices.recoveryRestart"
+
+    def perform(self, node, fullDeviceName):
+        """
+        Sends a restart message to a device for recovery.
+
+        :param node: node name
+        :type node: str
+        :param fullDeviceName: fully qualified device manager name
+        :type fullDeviceName: device
+        """
+        super(RecoveryRestart, self).perform(node, fullDeviceName, isRecovery=True)
