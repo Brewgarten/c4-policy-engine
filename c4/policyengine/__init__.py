4f39f21448952a994d0cbb24f2d02e622fbaa320
Mark A. Friend <mafriend@us.ibm.com>
2017-10-02 17:01:18 -0500

    Add OSS contribution prep requirements (#22)

--- c4/policyengine/__init__.py
+++ c4/policyengine/__init__.py
@@ -1,4 +1,8 @@
 """
+Copyright (c) IBM 2015-2017. All Rights Reserved.
+Project name: c4-policy-engine
+This project is licensed under the MIT License, see LICENSE
+
 A policy engine implementation with support for events and actions as well as textual representations
 """
 from pkgutil import extend_path
--- c4/policyengine/events/__init__.py
+++ c4/policyengine/events/__init__.py
@@ -1,4 +1,8 @@
 """
+Copyright (c) IBM 2015-2017. All Rights Reserved.
+Project name: c4-policy-engine
+This project is licensed under the MIT License, see LICENSE
+
 Policy events
 """
 from pkgutil import extend_path
